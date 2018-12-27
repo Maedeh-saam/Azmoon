93fa10f386ae504a72da7dd82a715945eda14fd2
maedeh <maedeh@maedehs-mac.local>
2018-12-27 17:06:23 +0330

    Initial Commit

---
+++ Azmoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Azmoon
+//
+//  Created by maedeh on 10/6/1397 AP.
+//  Copyright © 1397 maedeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AzmoonTests/AzmoonTests.swift
@@ -0,0 +1,34 @@
+//
+//  AzmoonTests.swift
+//  AzmoonTests
+//
+//  Created by maedeh on 10/6/1397 AP.
+//  Copyright © 1397 maedeh. All rights reserved.
+//
+
+import XCTest
+@testable import Azmoon
+
+class AzmoonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
